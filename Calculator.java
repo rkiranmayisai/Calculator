fe7e45333d2ce8d95c61669386809608b110f879
Reddy Kiranamayi <r.kiranmayisai@gmail.com>
2025-10-19 17:29:02 +0530

    Initial Commit

---
+++ Calculator.java
@@ -0,0 +1,35 @@
+import java.util.*;
+public class Calculator {
+  public static void main(String[] args) {
+    char op;
+    Double num1, num2, res;
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Choose an operator: +, -, *,/:");
+    op = sc.next().charAt(0);
+    System.out.println("Enter first number");
+    num1 = sc.nextDouble();
+    System.out.println("Enter second number");
+    num2 = sc.nextDouble();
+    switch (op) {
+      case '+':
+        res = num1 + num2;
+        System.out.println(num1 + " + " + num2 + " = " + res);
+        break;
+      case '-':
+        res = num1 - num2;
+        System.out.println(num1 + " - " + num2 + " = " + res);
+        break;
+      case '*':
+        res = num1 * num2;
+        System.out.println(num1 + " * " + num2 + " = " + res);
+        break;
+      case '/':
+        res = num1/ num2;
+        System.out.println(num1 + " / " + num2 + " = " + res);
+        break;
+      default:
+        System.out.println("Invalid operator!");
+        break;
+    }
+  }
+}
---
+++ Str.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class  Str{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        System.out.println("enter the value");
+        String str=sc.nextLine();
+        System.out.println(str+n);
+        sc.close();
+    }
+}
